def4545852b13938a4f70d7c5a2d81ed49caad97
hghe95 <hghe95@gmail.com>
2022-12-05 13:11:45 -0800

    added to resolvers and typedefs

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,4 +1,4 @@
-const { User, Book } = require(`../models`);
+const { User } = require(`../models`);
 const { signToken } = require(`../utils/auth`);
 const { AuthenticationError } = require(`apollo-server-express`); 
 
@@ -12,8 +12,48 @@ const resolvers = {
             }
             throw new AuthenticationError(`The user is not logged in`);
         }
+    },
+    Mutation: {
+        addUser: async (parent, args) => {
+            const user = await User.create(args);
+            const token = signToken(user);
+            return { user, token };
+        },
+        login: async (parent, { email, password }) => {
+            const user = await User.findOne( { email });
+            if (!user) {
+                throw new AuthenticationError(`Email or password was incorrect, please try again`)
+            }
+            const correctPass = await user.isCorrectPassword(password);
+            if(!correctPass) {
+                throw new AuthenticationError(`Email or password was incorrect, please try again`)
+            }
+            const token = signToken(user);
+            return { token, user };
+        },
+        saveBook: async (parent, args , context) => {
+            if (context.user) {
+                const updatedUser = await User.findOneAndUpdate(
+                    { _id: context.user_id },
+                    { $addToSet: { savedBooks: args.input }},
+                    { new: true }
+                )
+                return updatedUser;
+            }
+        },
+        removeBook: async (parent, args, context) => {
+            if (context.user) {
+                const updatedUser = await User.findOneAndUpdate(
+                    { _id: context.user_id },
+                    { $addToSet: { savedBooks: args.bookId }},
+                    { new: true }                    
+                );
+                return updatedUser;
+            }
+            throw new AuthenticationError(`The user must log in`)
+        }
+        
     }
-    
 }
 
 module.exports = resolvers;
