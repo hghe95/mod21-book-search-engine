98adcf2154bfb7f05b6f1cd7b9a080a9fce2f2d4
hghe95 <hghe95@gmail.com>
2022-12-06 15:32:24 -0800

    fixed typeDef and App issue

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,13 +1,22 @@
 const { gql } = require(`apollo-server-express`);
 
 const typeDefs = gql`
+    input SavedBookInput {
+        bookId: String
+        authors: [String]
+        description: String
+        title: String
+        image: String
+        link: String       
+    }
+
     type Query {
         me: User
     }
 
     type Mutation {
         login(email: String!, password: String!): Auth
-        addUser(username: String!), email: String!, password: String!): Auth
+        addUser(username: String!, email: String!, password: String!): Auth
         saveBook(book: SavedBookInput): User
         removeBook(bookId: String!): User
     }
@@ -15,7 +24,7 @@ const typeDefs = gql`
     type User {
         _id: ID
         username: String
-        email: String:
+        email: String
         bookCount: Int
         savedBooks: [Book]
     }
--- server/server.js
+++ server/server.js
@@ -26,8 +26,14 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
-app.use(routes);
+app.get(`*`, (req, res) => {
+  res.sendFile(path.join(__dirname, `../client/build/index.html`));
+});
+
+//app.use(routes);
 
 db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+  app.listen(PORT, () => {
+    console.log(`🌍 Please use GraphQL at http://localhost:${PORT}${StartApolloServer.graphqlPath}`);
+  });
 });
